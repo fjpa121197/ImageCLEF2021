6b49912dabd5726daa951231ab947ec05599eea3
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2021-03-09 14:04:35 -0600

    zzzzzaa

--- scripts/fetch_kaggle_dataset_2020.sh
+++ scripts/fetch_kaggle_dataset_2020.sh
@@ -1,4 +1,6 @@
 TRAINING_DATA_URL="fjpa121197/imageclef-med-concept-detection"
 NOW=$(date)
 
-kaggle datasets download -d $TRAINING_DATA_URL -p packages/cnn_modality_clf/cnn_modality_clf/datasets/
+kaggle datasets download -d $TRAINING_DATA_URL -p packages/cnn_modality_clf/cnn_modality_clf/datasets/ && \
+unzip packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclef-med-concept-detection.zip -d packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclef-med-concept-detection && \
+echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/cnn_modality_clf/cnn_modality_clf/datasets/training_data_reference.txt && \
