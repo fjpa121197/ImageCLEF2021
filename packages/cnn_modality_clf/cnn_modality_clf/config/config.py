0ca13c33c2456328d25bdb6760a3376b6327a8af
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2021-03-09 21:49:33 -0600

    fk me

--- packages/cnn_modality_clf/cnn_modality_clf/config/config.py
+++ packages/cnn_modality_clf/cnn_modality_clf/config/config.py
@@ -4,7 +4,7 @@ PWD = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_ROOT = os.path.abspath(os.path.join(PWD, '..'))
 DATASET_DIR = os.path.join(PACKAGE_ROOT, 'datasets')
 TRAINED_MODEL_DIR = os.path.join(PACKAGE_ROOT, 'trained_models')
-DATA_FOLDER = os.path.join(DATASET_DIR, 'imageclef-med-concept-detection/ImageCLEF2020_Training_Images')
+DATA_FOLDER = os.path.join(DATASET_DIR, 'imageclef-med-concept-detection/ImageCLEF2020_Training_Images/Train')
 
 # MODEL PERSISTING
 MODEL_NAME = 'cnn_modality_clf'
