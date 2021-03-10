c02ae55befbcdfbee1ab909f4b6353e184c15b0a
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2021-03-09 22:01:15 -0600

    kajskajskajs

--- packages/cnn_modality_clf/cnn_modality_clf/train_pipeline.py
+++ packages/cnn_modality_clf/cnn_modality_clf/train_pipeline.py
@@ -16,8 +16,7 @@ def run_training(save_result: bool = True):
     enc.fit(y_train)
     y_train = enc.transform(y_train)
 
-
-    pipe.pipe.fit(X_train, y_train)
+    pipe.fit(X_train, y_train)
 
     if save_result:
         joblib.dump(enc, config.ENCODER_PATH)
