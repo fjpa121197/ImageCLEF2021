bbf9b55e251891b7e1f349601ec33f732a10ee7a
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2021-03-09 19:09:14 -0600

    typo correction

--- packages/cnn_modality_clf/cnn_modality_clf/pipeline.py
+++ packages/cnn_modality_clf/cnn_modality_clf/pipeline.py
@@ -2,7 +2,7 @@ from sklearn.pipeline import Pipeline
 from cnn_modality_clf import model
 
 from cnn_modality_clf.config import config
-from cnn_modality_clf.processing import preprocessors as pp
+from cnn_modality_clf.preprocessing import preprocessors as pp
 
 
 pipe = Pipeline([
