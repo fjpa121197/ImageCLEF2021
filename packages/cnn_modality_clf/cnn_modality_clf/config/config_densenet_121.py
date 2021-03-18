169d5b63ed2691e108d3ce8f073e66d91fd36c9f
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2021-03-18 16:02:22 -0600

    this is it x2

--- packages/cnn_modality_clf/cnn_modality_clf/config/config_densenet_121.py
+++ packages/cnn_modality_clf/cnn_modality_clf/config/config_densenet_121.py
@@ -4,7 +4,7 @@ PWD = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_ROOT = os.path.abspath(os.path.join(PWD, '..'))
 DATASET_DIR = os.path.join(PACKAGE_ROOT, 'datasets')
 TRAINED_MODEL_DIR = os.path.join(PACKAGE_ROOT, 'trained_models')
-DATA_FOLDER = os.path.join(DATASET_DIR, 'imageclef-med-concept-detection/ImageCLEF2020_Training_Images/Train/')
+DATA_FOLDER = os.path.join(DATASET_DIR, 'imageclefmed2021/')
 
 # MODEL PERSISTING
 MODEL_NAME = 'densenet_121_modality_clf'
--- scripts/fetch_kaggle_dataset_2020.sh
+++ scripts/fetch_kaggle_dataset_2020.sh
@@ -3,5 +3,5 @@ SAMPLE_TRAINING_DATA_URL="fjpa121197/imageclef-sample"
 NOW=$(date)
 
 kaggle datasets download -d $SAMPLE_TRAINING_DATA_URL -p packages/cnn_modality_clf/cnn_modality_clf/datasets/ && \
-unzip packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclefmed2021.zip -d packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclef-med-concept-detection && \
+unzip packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclefmed2021.zip -d packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclefmed2021 && \
 echo $SAMPLE_TRAINING_DATA_URL 'retrieved on:' $NOW > packages/cnn_modality_clf/cnn_modality_clf/datasets/training_data_reference.txt
