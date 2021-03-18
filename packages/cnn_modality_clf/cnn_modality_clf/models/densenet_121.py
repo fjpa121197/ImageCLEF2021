7c217a56b39d5441eee94c2971657877f6eba66b
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2021-03-18 16:59:26 -0600

    ajsiajsiajs

--- packages/cnn_modality_clf/cnn_modality_clf/models/densenet_121.py
+++ packages/cnn_modality_clf/cnn_modality_clf/models/densenet_121.py
@@ -12,6 +12,7 @@ class DenseNetClassifier():
         self._optimizer = optimizers.Adam(learning_rate=0.001)
         self._metrics = ['acc']
         self._model = None
+        self._callbacks = [callbacks.EarlyStopping(monitor = 'val_loss', patience = 2, restore_best_weights = True)]
 
     def build_model(self):
         #Download pretrained DenseNet 121 model with imagenet weights (default) and image size
@@ -23,4 +24,15 @@ class DenseNetClassifier():
         prediction_layer = layers.Dense(7, activation= 'softmax')(model)
         self._model = models.Model(inputs = base_model.input, outputs = prediction_layer)
         print(self._model.summary())
+        self._model.compile(loss = self._loss, optimizer = self._optimizer, metrics = self._metrics)
+
+    def train(self, train_generator, validation_generator):
+
+        history = self._model.fit(train_generator, train_generator_1, steps_per_epoch=10, epochs=10,
+                                validation_data=validation_generator, callbacks = self._callbacks,
+                                validation_steps=2, verbose = 1)
+
+        return history
+
+
         
