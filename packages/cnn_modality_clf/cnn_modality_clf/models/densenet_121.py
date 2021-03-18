fa4644d12811e2bb7c46eca942856630b27859f0
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2021-03-18 16:36:57 -0600

    this is it x4

--- packages/cnn_modality_clf/cnn_modality_clf/models/densenet_121.py
+++ packages/cnn_modality_clf/cnn_modality_clf/models/densenet_121.py
@@ -5,35 +5,25 @@ from tensorflow.keras.applications import DenseNet121
 
 from config import config_densenet_121
 
-def densenet_121_model(image_size = 256, weights = 'imagenet'):
-    
-    #Download pretrained DenseNet 121 model with imagenet weights (default) and image size
-    base_model = DenseNet121(include_top = False, weights = weights, input_shape= (image_size,image_size,3))
-    model = layers.GlobalAveragePooling2D()(base_model.output)
-    model = layers.Dense(64, activation= 'relu')(model)
-    model = layers.Dropout(0.5, seed = 14)(model)
-    prediction_layer = layers.Dense(7, activation= 'softmax')(model)
-    model = models.Model(inputs = base_model.input, outputs = prediction_layer)
 
-    model.compile(loss = 'categorical_crossentropy', optimizer = optimizers.Adam(learning_rate = 0.001),
-                metrics = ['acc'])
+class DenseNetClassifier():
+    def __init__(self):
+        self._image_size = config_densenet_121.IMAGE_SIZE
+        self._weigths = 'imagenet'
+        self._loss = 'categorical_crossentropy'
+        self._optimizer = optimizers.Adam(learning_rate=0.001)
+        self._metrics = ['acc']
+        self._model = None
 
-    
-
-    return model
-
-
-earlystop = callbacks.EarlyStopping(monitor = 'val_loss', patience = 15, restore_best_weights= True)
-callbacks = [earlystop]
-
-cnn_clf = KerasClassifier(build_fn=densenet_121_model,
-                          validation_steps=10,
-                          epochs=config_densenet_121.EPOCHS,
-                          steps_per_epoch = config_densenet_121.EPOCHS,
-                          verbose=1,  # progress bar - required for CI job
-                          callbacks = [callbacks]
-                          )
-
-if __name__ == '__main__':
-    model = densenet_121_model()
-    model.summary()
+    def build_model(self):
+        #Download pretrained DenseNet 121 model with imagenet weights (default) and image size
+        base_model = DenseNet121(include_top = False, weights = self._weigths, 
+                                input_shape= (self._image_size,self._image_size,3))
+        model = layers.GlobalAveragePooling2D()(base_model.output)
+        model = layers.Dense(64, activation= 'relu')(model)
+        model = layers.Dropout(0.5, seed = 14)(model)
+        prediction_layer = layers.Dense(7, activation= 'softmax')(model)
+        self._model = models.Model(inputs = base_model.input, outputs = prediction_layer)
+        
+    def train(self):
+        
--- packages/cnn_modality_clf/cnn_modality_clf/train_pipeline.py
+++ packages/cnn_modality_clf/cnn_modality_clf/train_pipeline.py
@@ -4,7 +4,7 @@ from preprocessing import data_management as dm
 import sys
 
 
-def run_training(model: str, save_result: bool = True):
+def run_training(model_arg: str, save_result: bool = True):
 
     train_generator = dm.image_generator(data_path = config_densenet_121.DATA_FOLDER, 
                                         image_size = config_densenet_121.IMAGE_SIZE,
@@ -15,15 +15,13 @@ def run_training(model: str, save_result: bool = True):
                                             batch_size = config_densenet_121.BATCH_SIZE, subset = 'validation')
 
     print(densenet_121.cnn_clf.summary())
-    if model == 'densenet121':
-        densenet_121.cnn_clf.fit(train_generator,validation_data = validation_generator)
-    elif model == 'default':
-        pass
-    else:
-        pass
+    if model_arg == 'densenet121':
+        model = densenet_121.DenseNetClassifier().build_model()
+        print(model.summary())
+    
 
 
 if __name__ == '__main__':
-    model = sys.argv[1]
-    print(model)
-    run_training(model = model)
+    model_arg = sys.argv[1]
+    print(model_arg)
+    run_training(model_arg = model_arg)
