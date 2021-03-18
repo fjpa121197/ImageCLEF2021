f6913565b9c342462d7c8859a4b7ea9e96085b93
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2021-03-18 16:05:34 -0600

    this is it x3

--- scripts/fetch_kaggle_dataset_2020.sh
+++ scripts/fetch_kaggle_dataset_2020.sh
@@ -3,5 +3,5 @@ SAMPLE_TRAINING_DATA_URL="fjpa121197/imageclef-sample"
 NOW=$(date)
 
 kaggle datasets download -d $SAMPLE_TRAINING_DATA_URL -p packages/cnn_modality_clf/cnn_modality_clf/datasets/ && \
-unzip packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclefmed2021.zip -d packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclefmed2021 && \
+unzip packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclef-sample.zip -d packages/cnn_modality_clf/cnn_modality_clf/datasets/imageclef-sample && \
 echo $SAMPLE_TRAINING_DATA_URL 'retrieved on:' $NOW > packages/cnn_modality_clf/cnn_modality_clf/datasets/training_data_reference.txt
